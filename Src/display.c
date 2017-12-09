0bd525886dd47f8014553675fd2980b17ba768ef
Henri Merilä <henri.merila@fi.abb.com>
2017-12-09 17:13:22 +0200

    added some stuff to display logic

--- Src/display.c
+++ Src/display.c
@@ -4,13 +4,33 @@
  *  Created on: 22.11.2017
  *      Author: FIHEMER
  */
+#include <stdio.h>
+#include "ST7565.h"
+
 void drawTopInfo(void)
 {
+	// Write information text on top row
+	uint8_t line = 1;
+	uint8_t x = 0;
+	char c[80];
+	//int *volt = 0;
+	//int *temp = 0;
+	//int *charg = 0;
+	sprintf(c, "hello!");
+	ST7565_drawstring( x, line, c);
 
+	// Parameterize these to the right spot
+	uint8_t x0 = 0;
+	uint8_t y0 = 8;
+	uint8_t x1 = 0;
+	uint8_t y1 = 8;
+	uint8_t color = 1;
+
+	ST7565_drawline(x0, y0, x1, y1, color);
 	return;
 }
 
-void drawGraph(void)
+void drawGraph(int* values)
 {
 	return;
 }
@@ -23,7 +43,14 @@ void chargeView(void)
 	 * Also on the top side "Current values" are present
 	 */
 	drawTopInfo();
-	drawGraph();
+
+	char title[20] = "Charge view";
+	uint8_t line = 3;
+	uint8_t x = 20;
+	ST7565_drawstring(x , line, title);
+
+	// Collect data and draw graph
+	//drawGraph();
 }
 
 void voltageView(void)
@@ -33,7 +60,14 @@ void voltageView(void)
 	 * Also on the top side "Current values" are present
 	 */
 	drawTopInfo();
-	drawGraph();
+
+	char title[20] = "Voltage view";
+	uint8_t line = 3;
+	uint8_t x = 20;
+	ST7565_drawstring(x , line, title);
+
+	// Collect data and draw graph
+	//drawGraph();
 }
 
 void temperatureView(void)
@@ -43,6 +77,12 @@ void temperatureView(void)
 	 * Also on the top side "Current values" are present
 	 */
 	drawTopInfo();
-	drawGraph();
-}
 
+	char title[20] = "Temperature view";
+	uint8_t line = 3;
+	uint8_t x = 20;
+	ST7565_drawstring(x , line, title);
+
+	// Collect data and draw graph
+	//drawGraph();
+}
